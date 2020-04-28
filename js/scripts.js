c7034873b1467485925a0012c04fdbb0b66151a3
me <me@me.me.com>
2020-04-28 13:56:29 -0700

    add recursion

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,21 @@ function factorial(factorialValue){
   return result;
 }
 
+function factorialRecursion(factorialValue){
+  if (factorialValue === 1){
+    return 1;
+  }
+  return factorialValue * factorialRecursion(factorialValue - 1);
+}
+
 
 $(document).ready(function(){
   $("form").submit(function(e){
     e.preventDefault();
     var factorialValue = parseInt($("input").val());
     var result = factorial(factorialValue);
+    var result2 = factorialRecursion(factorialValue);
     $("#result").text(result);
+    $("#result2").text(result);
   });
 });
